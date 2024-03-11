335a8310855d87de6b4027828296bd52e7a9b113
sahil bishnoi <sahilbishnoi156@gmail.com>
2024-03-11 23:52:43 +0530

    added sketch route

---
+++ src/app/sketch/page.tsx
@@ -0,0 +1,8 @@
+import MainCanvas from '@/components/sketchComps/MainCanvas'
+import React from 'react'
+
+export default function page() {
+  return (
+    <MainCanvas/>
+  )
+}
---
+++ src/components/sketchComps/MainCanvas.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function MainCanvas() {
+  return (
+    <div>Page is still developing</div>
+  )
+}
